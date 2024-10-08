41df096f6a11ec4bbeaec4b27896148f22595bba
bradcstevens <brad@bradcstevens.com>
2024-10-08 00:44:14 +0000

    updated suffixes

--- setup.sh
+++ setup.sh
@@ -7,5 +7,5 @@ az provider register -n Microsoft.Chaos -c
 az ad sp create-for-rbac \
     -n contosotraders-sp \
     --role Owner \
-    --scopes /subscriptions/3523b0e6-bb53-4e87-8340-25c416e26093 \
+    --scopes /subscriptions/$AZ_SUBSCRIPTION_ID \
     --sdk-auth
